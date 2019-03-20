19c6d11cfcea26ed639265822e36c379faafbad3
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-03-20 21:25:19 +0300

    add js file

---
+++ uppercase.js
@@ -0,0 +1,14 @@
+
+let text=prompt("Введите текст")
+function upperCase() {
+	let textArray=text.split('')
+	for(let i=-1; i<textArray.length; i++){
+	if(textArray[i]==" "|| i===-1){
+
+		textArray[i+1]=textArray[i+1].toUpperCase()
+	}
+	}
+	return textArray.join('')
+	}
+	alert(upperCase())
+
