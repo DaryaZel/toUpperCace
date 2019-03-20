95e9a90b1aa45e6d39716ab6db410e0ef26f3070
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-03-21 01:24:24 +0300

    update uppercase.js

--- uppercase.js
+++ uppercase.js
@@ -1,14 +1,14 @@
+let text = prompt("Введите текст")
 
-let text=prompt("Введите текст")
 function upperCase() {
-	let textArray=text.split('')
-	for(let i=-1; i<textArray.length; i++){
-	if(textArray[i]==" "|| i===-1){
+    let textArray = text.split('')
+    for (let i = -1; i < textArray.length; i++) {
+        if (textArray[i] == " " || i === -1) {
 
-		textArray[i+1]=textArray[i+1].toUpperCase()
-	}
-	}
-	return textArray.join('')
-	}
-	alert(upperCase())
+            textArray[i + 1] = textArray[i + 1].toUpperCase()
+        }
+    }
+    return textArray.join('')
+}
+alert(upperCase())
 
